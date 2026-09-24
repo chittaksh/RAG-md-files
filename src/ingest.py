440cf14b0354193b34f2be1b424e2cbabc8ac8bf
Chittaksh Khadse <khadse.chittaksh@gmail.com>
2026-09-24 18:10:34 +0530

    updated ingest for incomplete answers

--- src/ingest.py
+++ src/ingest.py
@@ -47,7 +47,7 @@ def load_docs(data_path: Path):
 
 def split_docs(docs):
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 500, ##approx size of each chunk is 500
+        chunk_size = 1000, ##approx size of each chunk is 1000
         chunk_overlap = 50
     )
     return splitter.split_documents(docs)
